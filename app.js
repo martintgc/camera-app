a591f79e2c17fb25b9951cd22615b33f2b45745c
martintgc <martin.schwarz@tgc-solutions.com>
2019-11-13 15:12:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ function processVideo() {
 setTimeout(processVideo, 0);
   
 */
-let height=cameraView.videoHeight;
-let width=cameraView.videoWidth;
+var height=cameraView.videoHeight;
+var width=cameraView.videoWidth;
 let src = new cv.Mat(height, width, cv.CV_8UC4);
 let dst = new cv.Mat(height, width, cv.CV_8UC1);
 let cap = new cv.VideoCapture(cameraView);
