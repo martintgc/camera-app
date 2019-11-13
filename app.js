28f7395cb326f7f70255912a74f17fa9ce198b46
martintgc <martin.schwarz@tgc-solutions.com>
2019-11-13 14:15:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -74,8 +74,9 @@ cameraTrigger.onclick = function() {
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    */
+    
     cameraOutput.classList.add("taken");
+    */
     // track.stop();
 };
 
