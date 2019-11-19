1f50c7431463ccc5e5edad9780bfc43bc70ae60c
martintgc <martin.schwarz@tgc-solutions.com>
2019-11-19 11:25:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -105,6 +105,7 @@ function checkFrame() {
 		perimeter=cv.arcLength(cnt, true);
 		cv.approxPolyDP(cnt, cnt_tmp, 0.03 * perimeter, true);
 		console.log(cnt_tmp.rows);
+		console.log(cnt_tmp[0]);
 		if (cv.isContourConvex(cnt_tmp) && (cnt_tmp.rows==4) && (maxAreaFound < cv.contourArea(cnt_tmp)) 
 		   && (cv.contourArea(cnt_tmp)< MAX_CONTOUR_AREA) 
 		   && (cv.contourArea(cnt_tmp)<requiredArea) ) {
